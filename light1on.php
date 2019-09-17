b9ed6cbfda99d03250441bd2d7fc31426fc2beb3
panyapatts <55220122+panyapatts@users.noreply.github.com>
2019-09-17 12:24:06 +0700

    Update light1on.php

--- light1on.php
+++ light1on.php
@@ -1,13 +1,41 @@
 <?php
-$data = "{'sub': 'field_value'}";
-$url = "https://tdliff.firebaseio.com/Clip1.json";
-$headers = array('Content-Type: application/json');
+// Constants
+$FIREBASE = "https://tdliff.firebaseio.com/Clip1.json";
+$NODE_DELETE = "temperature.json";
+$NODE_GET = "temperature.json";
+$NODE_PATCH = ".json";
+$NODE_PUT = "temperature.json";
+// Data for PUT
+// Node replaced
+$data = 32;
+// Data for PATCH
+// Matching nodes updated
+$data = array(
+    "Sub2" => 42
+);
+// JSON encoded
+$json = json_encode( $data );
+// Initialize cURL
 $curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PATCH');
-curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-$response = curl_exec($curl);
-curl_close($curl);
+// Create
+// curl_setopt( $curl, CURLOPT_URL, $FIREBASE . $NODE_PUT );
+// curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "PUT" );
+// curl_setopt( $curl, CURLOPT_POSTFIELDS, 32 );
+// Read
+// curl_setopt( $curl, CURLOPT_URL, $FIREBASE . $NODE_GET );
+// Update
+curl_setopt( $curl, CURLOPT_URL, $FIREBASE . $NODE_PATCH );
+curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "PATCH" );
+curl_setopt( $curl, CURLOPT_POSTFIELDS, $json );
+// Delete
+// curl_setopt( $curl, CURLOPT_URL, $FIREBASE . $NODE_DELETE );
+// curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "DELETE" );
+// Get return value
+curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+// Make request
+// Close connection
+$response = curl_exec( $curl );
+curl_close( $curl );
+// Show result
+echo $response . "\n";
 ?>
